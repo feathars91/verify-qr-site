dd4886f3c392f1a390c5dfd8f0a1b11bf3caf781
Feadars <abelino.chinchilla@gmail.com>
2021-09-24 18:19:46 -0500

    auth2

--- amplify/backend/function/authlambda/src/index.js
+++ amplify/backend/function/authlambda/src/index.js
@@ -1,28 +1,25 @@
-
-
+var x;
 exports.handler = async (event) => {
-    // TODO implement
+  // TODO implement
 
-if(event.requestContext.authorizer){
-const user = event.requestContext.authorizer.claims
-console.log('claims: ' , event.requestContext.authorizer.claims)
+  if (event.requestContext.authorizer) {
+    const user = event.requestContext.authorizer.claims;
+    console.log("claims: ", event.requestContext.authorizer.claims);
+    x = user;
 
-console.log('userslaims: ' , user)
-}
-else{
-console.log('no claims ')
+    console.log("userslaims: ", user);
+  } else {
+    console.log("no claims ");
+  }
 
-}
-    
-
-    const response = {
-        statusCode: 200,
+  const response = {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
-      headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Acess-Control-Allow-Headers": "*"
-      }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Acess-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify(x),
+  };
+  return response;
 };
